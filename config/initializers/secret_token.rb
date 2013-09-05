902735d11a3ff7e571ab0b90216daf96bd86507b
ghar1821 <ghar1821@gmail.com>
2013-09-05 22:11:30 +1000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sensoriumhealth::Application.config.secret_key_base = 'ac7810512cedd2148412e766e90c0d290faa63837b2d170e38f6f96625a2ac28e779137f000b8850e09f4da157c19699d3f8b27db2fd6b03bafef59cb7a99485'
