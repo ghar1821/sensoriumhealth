37987229f9313b525d1b323a301cde0eeb6ffe9f
ghar1821 <ghar1821@gmail.com>
2013-09-15 19:52:26 +1000

    validation for registering users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-    has_many :microposts
+    validates :firstname,   presence: true
+    validates :lastname,   presence: true
+    validates :email,   presence: true
+    validates :username,   presence: true
+    validates :dob,   presence: true
+    validates :gender,   presence: true
+    validates :city,   presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130906073048) do
+ActiveRecord::Schema.define(version: 20130914121632) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -21,8 +21,13 @@ ActiveRecord::Schema.define(version: 20130906073048) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "name"
+    t.string   "firstname"
+    t.string   "lastname"
     t.string   "email"
+    t.string   "username"
+    t.date     "dob"
+    t.boolean  "gender"
+    t.string   "city"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
