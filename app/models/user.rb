db6af81a9ee4849869ce13733302e9de4872094f
ghar1821 <ghar1821@gmail.com>
2013-09-16 00:10:08 +1000

    more validation on email and names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-    validates :firstname,   presence: true
-    validates :lastname,   presence: true
+    validates :firstname,   presence: true, length: { maximum: 30 }
+    validates :lastname,   presence: true,  length: { maximum: 30 }    
     validates :email,   presence: true
-    validates :username,   presence: true
+    validates :username,   presence: true,  length: { maximum: 30 }
     validates :dob,   presence: true
     validates :gender,   presence: true
     validates :city,   presence: true
