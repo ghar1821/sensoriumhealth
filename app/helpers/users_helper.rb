26db94ef1710459eecdb6ea863c9e0cf4da4107a
ghar1821 <ghar1821@gmail.com>
2013-09-19 01:53:27 +1000

    Added gravatar to user's helper fu

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,14 @@
 module UsersHelper
 
     # Returns the Gravatar (http://gravatar/com/) for the given user.
+    # @param: user object.
+    # @return: image tag for Gravatar with a "gravatar" class
     def gravatar_for(user)
         # Gravatar URLs are based on an MD5 hash of the user's email address.
         # Ruby's MD5 hashing algorithm is implemented using the hexdigest method.
         # Hexdigest method is part of Digest library.
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-        image_tage
+        image_tag(gravatar_url, alt: user.username, class: "gravatar")
+    end
 end
