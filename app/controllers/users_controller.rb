8ea63b2113d84edb487814bc8aae1426441f5838
ghar1821 <ghar1821@gmail.com>
2013-09-20 01:26:39 +1000

    Partial form resubmission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    # Use the form submission to create new user object using User.new,
+    # Try and fail to save that user,
+    # then render the signup page for possible resubmission.
+    def create
+        @user = User.new(params[:user]) # Not final!
+        if @user.save
+            # successful save
+        else
+            render 'new'
+        end
+    end
 end
