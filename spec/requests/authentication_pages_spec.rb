35267501f63fa19bca505dc42dd5ba999b3b0a5a
ghar1821 <ghar1821@gmail.com>
2013-09-22 01:01:41 +1000

    added test code for signin process

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,4 +8,31 @@ describe "Authentication" do
         it { should have_content('Sign in') }
         it { should have_title('Sign in') }
     end
+
+    desribe "signin" do
+        before { visit signin_path }
+   
+        # Sigin failure.
+        describe "with invalid information" do
+            before { click_button "Sign in" }
+
+            it { should have_title('Sign in') }
+            it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+        end
+
+        # Sigin successful
+        describe "with valid information" do
+            let(:user) { FactoryGirl.create(:user) }
+            before do
+                fill_in "Username",     with: user.username
+                fill_in "Password",     with: user.password
+                click_button "Sign in"
+            end
+
+            it { should have_title(user.username) }
+            it { should have_link('Profile',    href: user_path(user)) }   
+            it { should have_link('Sign out',  href: signout_path) }
+            it { should_not have_link('Sign in',   href: signin_path) }    
+        end
+    end
 end
