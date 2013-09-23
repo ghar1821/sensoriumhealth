9acef70cab0e0c29b29809648a0c559b947dfddc
dlat8367 <dlat8367@uni.sydney.edu.au>
2013-09-24 00:07:05 +1000

    need to define array properly

--- app/controllers/relaxation_sessions_controller.rb
+++ app/controllers/relaxation_sessions_controller.rb
@@ -1,4 +1,13 @@
 class RelaxationSessionsController < ApplicationController
-    @xAxis = ["","","","","","","","","",""]
-    @yAxis = [64,67,65,66,64,67,70,72,68,69]
+    @xAxis = ['1','2','3','4','5','6','7','8','9','10']
+    @yAxis = ['64','67','65','66','64','67','70','72','68','69']
+    
+    def index
+        @xAxis = ['1','2','3','4','5','6','7','8','9','10']
+        respond_to do |format|
+            format.html # index.html.erb
+            format.json { render :json => @xAxis}
+            format.json { render :json => @xAxis}
+        end
+    end
 end
