e70456064b56642fb6659900894d0a07f9268588
dlat8367 <dlat8367@uni.sydney.edu.au>
2013-09-24 17:46:12 +1000

    Passed varibles to javascript in view

--- app/controllers/relaxation_sessions_controller.rb
+++ app/controllers/relaxation_sessions_controller.rb
@@ -1,9 +1,8 @@
 class RelaxationSessionsController < ApplicationController
-    @xAxis = ['1','2','3','4','5','6','7','8','9','10']
-    @yAxis = ['64','67','65','66','64','67','70','72','68','69']
-    
     def index
-        @xAxis = ['1','2','3','4','5','6','7','8','9','10']
+        @xAxis = [1,2,3,4,5,6,7,8,9,10]
+        @yAxis = [64,67,65,66,64,67,70,72,68,69]
+        
         respond_to do |format|
             format.html # index.html.erb
             format.json { render :json => @xAxis}
