b8ec85d4542d2f61247e766361191fccc39b12ca
dlat8367 <dlat8367@uni.sydney.edu.au>
2013-09-24 17:59:26 +1000

    editing view

--- app/controllers/relaxation_sessions_controller.rb
+++ app/controllers/relaxation_sessions_controller.rb
@@ -1,7 +1,7 @@
 class RelaxationSessionsController < ApplicationController
     def index
-        @xAxis = [1,2,3,4,5,6,7,8,9,10]
-        @yAxis = [64,67,65,66,64,67,70,72,68,69]
+        @xAxis = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+        @yAxis = [64,67,65,66,64,67,70,72,68,69,73,70,69,63,63]
         
         respond_to do |format|
             format.html # index.html.erb
