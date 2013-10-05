4939e97ceba9df4ac3fabf3af19b538371054346
dlat8367 <dlat8367@uni.sydney.edu.au>
2013-10-05 19:27:28 +1000

    limit query

--- app/controllers/session_heart_rates_controller.rb
+++ app/controllers/session_heart_rates_controller.rb
@@ -1,6 +1,7 @@
 class SessionHeartRatesController < ApplicationController
     def index
         # Display data from the relaxation_session table
-        @session_heart_rate = SessionHeartRate.all
+        @session_heart_rate = SessionHeartRate.find(:all, :limit => 100)
+        #@session_heart_rate = SessionHeartRate.all
     end
 end
