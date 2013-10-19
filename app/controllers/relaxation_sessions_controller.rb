90f831e7a089b3d0752e95dde46bb9d30ec8c707
dlat8367 <dlat8367@uni.sydney.edu.au>
2013-10-19 12:35:50 +1100

    delete old graphs

--- app/controllers/relaxation_sessions_controller.rb
+++ app/controllers/relaxation_sessions_controller.rb
@@ -1,19 +1,3 @@
 class RelaxationSessionsController < ApplicationController
-    require 'fourier'
-    require 'numbers'
-    
-    def graph
-        # Create new data. Data is an array of IBIs. This will be replaced this with data fetched from the real DB.
-        @data = ::Numbers.get_data
-        # Fast Fourier Transform of IBI data array
-        @fft = ::Fourier.fft(@data)
-        # Returns magnitude
-        @yAxis = ::Fourier.to_magnitude(@fft)
-        
-        respond_to do |format|
-            format.html # index.html.erb
-            format.json { render :json => @xAxis}
-            #format.json { render :json => @xAxis}
-        end
-    end
+
 end
